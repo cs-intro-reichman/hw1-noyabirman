32e3d0bbe1100d453d9951e774034de16d51e33f
Noya Birman <noya.birman@post.runi.ac.il>
2024-11-08 18:52:28 +0200

    completed

--- Bill3.java
+++ Bill3.java
@@ -1,6 +1,6 @@
 class Bill3 {
 	public static void main(String[] args){
-	String name1= args[0];
+	String name1= args[0]; //recieving input from the user
 	String name2= args[1];
 	String name3= args[2];
 	int bill= Integer.parseInt(args[3]);

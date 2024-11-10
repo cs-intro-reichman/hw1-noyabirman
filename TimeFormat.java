36973f6981b86a1625ec2f82f4d171225cf02584
Noya Birman <noya.birman@post.runi.ac.il>
2024-11-10 12:15:54 +0200

    completed

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,6 +12,19 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
-	}
+		String period;
+		if (hours>=12){
+			period= "pm";
+			if (hours>12) {
+			hours-=12;
+			}
+		} else {
+			period= "am";
+			if (hours==0){
+				hours=12;
+			}
+		}
+		System.out.println(hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + " " + period);
+		
+	}	
 }
